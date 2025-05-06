ea687c8b5a43f2a23e2ba8c110db7d5a9c6f2fa6
Notzeiro <notzeiro@gmail.com>
2025-05-06 16:16:05 -0400

    tuve una clase

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,9 +1,14 @@
-users=["user1", "user2", "user3", "notzeiro"]
-passwords=["1234","4321","1111","catita"]
-bank=[500000 , 0 , 100000, 350000]
+users=["user1", "user2", "user3", "notzeiro", "heterias"]
+passwords=["1234","4321","1111","catita","hola123"]
+bank=[500000 , 0 , 100000 , 350000 , 10000000]
 access=True
 trueaccess=False
 tries=4
+
+
+def showbalance():
+  number=users.index(selected_user)
+  print (bank[number])
 
 #this below is the login system
 
@@ -34,45 +39,56 @@ while access==True:
      print ("User misspelled or non existent, please try again")
 
 if trueaccess==True: #From here starts the actual program
-
-K5=30         #amount of bills in the atm
-K10=30
-K20=30
-p=True
-while p=True:
- menu=int(input("1.-Show your balance\n2.-Withdraw\n3.-Deposit\n0.-Exit")) #ATM menu
+ number=users.index(selected_user)
+ usersbalance=(bank[number])
+ K5=30         #amount of bills in the atm
+ K10=30
+ K20=30
+ p=True
+while p==True:
+ menu=int(input("1.-Show your balance\n2.-Withdraw\n3.-Deposit\n0.-Exit\n")) #ATM menu
  if menu==0:
   print("Bye ^^") #exit 
   break
  elif menu==1:
-  print(f"{trueuser}'s balance is {bank.index(users.index(trueuser))}") #user balance show
+  print (f"{selected_user}s balance is:")
+  showbalance()
+  print("")
  elif menu==2:
   wanted_withdraw=int(input("How much would you like to withdraw?\n"))
-  if wanted_withdraw>(bank.index(users.index(trueuser))):
-    print("insufficient funds")  
-  elif wanted_withdraw<(bank.index(users.index(trueuser))):
+  if wanted_withdraw>(bank[number]):
+    print("\ninsufficient funds :[\nPlease select another option:\n")  
+  elif wanted_withdraw<(bank[number]):
     
 
-    K20t=(wanted_withdraw//20000) #basic structure to sort the bills that will be given to the user
-    wanted_withdraw-=(K20t*20000)
-    K10t=(wanted_withdraw//10000)
-    wanted_withdraw-=(K10t*10000)
-    K5t=(wanted_withdraw//5000)
-    wanted_withdraw-=(K5t*5000)
+    # K20t=(wanted_withdraw//20000) #basic structure to sort the bills that will be given to the user
+    # wanted_withdraw-=(K20t*20000)
+    # K10t=(wanted_withdraw//10000)
+    # wanted_withdraw-=(K10t*10000)
+    # K5t=(wanted_withdraw//5000)
+    # wanted_withdraw-=(K5t*5000)
 
-    K20-=K20t #this subtracts the given bills from the ATM
-    K10-=K10t
-    K5-=K5t
+    # K20-=K20t #this subtracts the given bills from the ATM
+    # K10-=K10t
+    # K5-=K5t
+    usersbalance-=wanted_withdraw
 
-    (bank.index(users.index(trueuser)))-=wanted_withdraw #this subtracts the withdrawed money from the users balance
+    (bank[number])=usersbalance
 
+    print(f"You withdrawed {wanted_withdraw} from your account\nyour new balance is {usersbalance}\n")
+    #amount_to_substract=(bank[number])-wanted_withdraw #this is the amount that will be substracted from the user balance
 
 
 
     _20kbills=[]
     _10kbills=[]
     _5kbills=[]
-    for h in range 30:
+    for h in range (30):
       _20kbills.append(20000)
       _10kbills.append(10000)
       _5kbills.append(5000)
+ elif menu==3:
+    wanted_deposit=int(input("How much will you deposit?\n"))   
+    usersbalance+=wanted_deposit
+    (bank[number])=usersbalance
+    print(f"You deposited {wanted_deposit} to your account\nyour new balance is {usersbalance}\n")
